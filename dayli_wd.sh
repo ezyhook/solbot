2cbe8718dda0df8fc635f806b90f19eb408172be
ezyhook <113023731+ezyhook@users.noreply.github.com>
2023-08-01 17:12:42 +0300

    Create dayli_wd.sh

---
+++ dayli_wd.sh
@@ -0,0 +1,13 @@
+set -e
+BOT_TOKEN=
+CHAT_ID=""
+
+vote_account="/root/solana/vote-account-keypair.json"
+withdrawer="/root/solana/authorized-withdrawer-keypair.json"
+identity_account="/root/solana/validator-keypair.json"
+wd=$(solana withdraw-from-vote-account --authorized-withdrawer $withdrawer $vote_account $identity_account 3)
+sleep 1
+balance=$(solana balance)
+curl --header 'Content-Type: application/json' --request 'POST' --data '{"chat_id":"'"$CHAT_ID"'","text":"<b>🟢 Withdrawed</b><code>
+'"$wd"'
+<b>Balance: '"$balance"'</b></code>",  "parse_mode": "html"}' "https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
